847d55bcc2bab822597b39dd6bab881ea5a295c9
pherphepheni <46635978+pherphepheni@users.noreply.github.com>
2019-01-28 17:31:40 -0300

    Updates

---
+++ precache-manifest.1b822c8b3e6706f75bff481e634cc95e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c48537af9afe0d7c79c",
+    "url": "/clima/static/js/runtime~main.6c48537a.js"
+  },
+  {
+    "revision": "720de35bc12a669b1488",
+    "url": "/clima/static/js/main.720de35b.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/clima/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "720de35bc12a669b1488",
+    "url": "/clima/static/css/main.85cf701f.chunk.css"
+  },
+  {
+    "revision": "dd34884cb09f88401a5f20be8c734637",
+    "url": "/clima/index.html"
+  }
+];
